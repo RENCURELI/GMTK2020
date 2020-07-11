5560ab159113d56e878835da0ba9b50093e76c03
RENCURELI <pierre.rencurel@orange.fr>
2020-07-11 10:03:10 -0400

    change to folder architecture
    DONE : Changed a few folders and working on state implimentation
    TODO : Finalize state implementation and start behaviour definition

---
+++ Assets/_Scripts/NPC/States/Idle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "State Idle", menuName = "State Machine/States/Idle")]
+public class Idle : State
+{
+    public override void InitState()
+    {
+        //stateMachine.GetComponent<Behaviour_Idle>().enabled = true;
+    }
+
+    public override void UpdateState()
+    {
+        /*if (stateMachine.GetComponent<Behaviour_Idle>().completed)
+        {
+            stateMachine.GetComponent<Behaviour_Idle>().completed = false;
+            stateMachine.GetComponent<Behaviour_Idle>().enabled = false;
+            ExitState();
+        }*/
+    }
+
+    public override void ExitState()
+    {
+        ExitToTransition(0);
+    }
+}
--- Assets/_Scripts/NPC/States/MoveTo.cs
+++ Assets/_Scripts/NPC/States/MoveTo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTo : MonoBehaviour
+public class MoveTo : State
 {
     // Start is called before the first frame update
     void Start()
