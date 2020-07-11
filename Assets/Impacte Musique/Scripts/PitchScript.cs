2ef9570e97376062d271bd053fb96b7510d3432e
TartifletteVEVO <56391305+TartifletteVEVO@users.noreply.github.com>
2020-07-11 20:04:01 +0200

    Gestion de l'impact sur la musique
    Première version
    - Creation du Script de deformation de la musique sur une Track d'AudioMixer
    - Vibrato et Baisse de Volume DONE
    - Modification du BPM à prévoir

---
+++ Assets/Impacte Musique/Scripts/PitchScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PitchScript : MonoBehaviour
+{
+    public AudioMixer MainMixer;
+    bool condition_vibrato;
+    bool condition_volume;
+    bool condition_bpm;
+    float timer;
+    float volume_inc;
+    float bpm_inc;
+
+    void Start()
+    {
+        condition_vibrato = false;
+        condition_volume = false;
+        condition_bpm = false;
+        timer = 0;
+        volume_inc = 0;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (condition_volume == true)
+            Volume();
+        else
+            MainMixer.SetFloat("volumeval", 0);
+        if (condition_vibrato == true)
+            Vibrato();
+        else
+            MainMixer.SetFloat("pitchval", 1);
+    }
+
+    public void Vibrato_Switch()
+    {
+        condition_vibrato = !condition_vibrato;
+    }
+    public void Volume_Switch()
+    {
+        condition_volume = !condition_volume;
+    }
+
+    public void BPM_Switch()
+    {
+        condition_bpm = !condition_bpm;
+    }
+
+    void Vibrato()
+    {
+        MainMixer.SetFloat("pitchval", 1 + oscillate(timer, 5, 0.50f));
+    }
+
+    void Volume()
+    {
+        volume_inc -= 0.01f;
+        MainMixer.SetFloat("volumeval", volume_inc);
+    }
+
+    float oscillate(float time, float speed, float scale)
+    {
+        return Mathf.Cos(time * speed / Mathf.PI) * scale;
+    }
+}
