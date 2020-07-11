5da3138c6f8734fab52963ced2ab53da8dc6acf3
RENCURELI <pierre.rencurel@orange.fr>
2020-07-11 09:42:19 -0400

    State machine setup
    DONE : Created virtual state class and base state machine class
    TODO : Create first state implementations and test

--- Assets/_Scripts/NPC/State.cs
+++ Assets/_Scripts/NPC/State.cs
@@ -2,17 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class State : MonoBehaviour
+[System.Serializable]
+public abstract class State : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.NonSerialized]
+    public StateMachine stateMachine; //State machine the instance belongs too
+
+    [System.NonSerialized]
+    public State firstState; //Start state
+
+    /// <summary>
+    /// Manage state initialization and startup
+    /// </summary>
+    public virtual void InitState() { }
+
+    /// <summary>
+    /// Manage state Update
+    /// </summary>
+    public virtual void UpdateState() { }
+
+    /// <summary>
+    /// Manage state transition, exit and disabling
+    /// </summary>
+    public virtual void ExitState() { }
+
+    public virtual void OnStateMachineDisable() { }
+
+    /// <summary>
+    /// Move to next state
+    /// </summary>
+    public void ExitToTransition(int index)
     {
-        
+        StateMachine.Transitions transition = GetTransition(index);
+        stateMachine.activeState.ExitState();
+        if (transition != null)
+        {
+            transition.stateMachine.EnterState(transition.state);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Select next state
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public StateMachine.Transitions GetTransition(int index)
     {
-        
+        StateMachine.StatePair pair = stateMachine.GetPairFromState(firstState);
+        if (pair.transitions.Count <= index)
+        {
+            return null;
+        }
+        StateMachine.Transitions transitions = pair.transitions[index];
+        return transitions;
     }
+
 }
--- Assets/_Scripts/NPC/StateMachine.cs
+++ Assets/_Scripts/NPC/StateMachine.cs
@@ -4,15 +4,118 @@ using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+ #region Variables
+    public class Transitions
     {
-        
+        public StateMachine stateMachine;
+        public State state;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Pair a state with a following state (using a list for multiple followup state scenarios)
+    /// </summary>
+    public class StatePair
     {
-        
+        public State state;
+        public List<Transitions> transitions = new List<Transitions>();
     }
+
+    /// <summary>
+    /// List of all states on the current FSM
+    /// </summary>
+    public List<StatePair> allStates;
+    /// <summary>
+    /// FSM Starting state
+    /// </summary>
+    public State startingState;
+    /// <summary>
+    /// Currently active state
+    /// </summary>
+    public State activeState;
+
+#endregion
+
+#region Methods
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Start()
+    {
+        EnterState(startingState);
+    }
+
+    /// <summary>
+    /// Initialize states used by FSM
+    /// </summary>
+    void Init()
+    {
+        var stateToCopy = new Dictionary<State, State>();
+        
+        foreach (StatePair pair in allStates)
+        {
+            //If potential state not in dictionnary, add state to dictionnary
+            if (!stateToCopy.ContainsKey(pair.state))
+            {
+                var m_temp = Instantiate(pair.state);
+                m_temp.firstState = pair.state;
+                m_temp.stateMachine = this;
+
+                stateToCopy.Add(pair.state, m_temp);
+            }
+            pair.state = stateToCopy[pair.state];
+        }
+    }
+
+    public StatePair GetPairFromState(State state)
+    {
+        foreach (StatePair pair in allStates)
+        {
+            if (pair.state.firstState == state)
+            {
+                return pair;
+            }
+        }
+        return null;
+    }
+
+    public void EnterState(State state)
+    {
+        foreach (StatePair pair in allStates)
+        {
+            if (pair.state.firstState == state)
+            {
+                pair.state.InitState();
+                activeState = pair.state;
+                return;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (activeState != null)
+        {
+            activeState.UpdateState();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (activeState != null)
+        {
+            activeState.OnStateMachineDisable();
+            activeState = null;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (activeState == null)
+        {
+            EnterState(startingState);
+        }
+    }
+#endregion
 }
