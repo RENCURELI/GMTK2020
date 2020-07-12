54c5d20c3823cbb3c7dbee46a37743f14f2678b5
TartifletteVEVO <56391305+TartifletteVEVO@users.noreply.github.com>
2020-07-12 09:57:52 +0200

    première phase des emotions
    Creation du script émotionnel de groupe et personnel
    - Setter DONE
    - Algorithme + timer A FINIR
    - Split tracking A FINIR

---
+++ Assets/Emotions/Scripts/AIGroupBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIGroupBehaviourScript : MonoBehaviour
+{
+    public GameObject BasseAI;
+    public GameObject PianoAI;
+    public GameObject BatterieAI;
+    public GameObject GuitareAI;
+    public GameObject ViolonAI;
+    public GameObject SaxoAI;
+
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+}
---
+++ Assets/Emotions/Scripts/AiBehaviourScript.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public enum Status
+{
+    AMI = 0,
+    INDIFFERENT = 1,
+    ENNEMI = 2
+}
+
+public class AiBehaviourScript : MonoBehaviour
+{
+    int probvalue;
+    public Status statusAI1;
+    public Status statusAI2;
+    public Status statusAI3;
+    public Status statusAI4;
+    public Status statusAI5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        probvalue = 50;
+        SetStatus();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetStatus()
+    {
+        statusAI1 = StatusRandomizer();
+        print(statusAI1);
+        statusAI2 = StatusRandomizer();
+        print(statusAI2);
+        statusAI3 = StatusRandomizer();
+        print(statusAI3);
+        statusAI4 = StatusRandomizer();
+        print(statusAI4);
+        statusAI5 = StatusRandomizer();
+        print(statusAI5);
+    }
+
+    Status StatusRandomizer()
+    {
+        return (Status)UnityEngine.Random.Range(0, 3);
+    }
+}
